c5011e0bef9c9c273ce5f1d73808188c44f0534b
brewchetta <chett.tiller@gmail.com>
2019-01-10 11:42:54 -0500

    Fixed minor typo that led to issues

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,11 +1,11 @@
-class API::V1::AuthController < ApplicationController
+class Api::V1::AuthController < ApplicationController
   before_action :find_user
   skip_before_action :authorized, only: [:create]
 
   def create
     if @user && @user.authenticate(user_login_params[:password])
       @token = encode_token( user_id: @user.id )
-      render json: { user: @user, jwt: token }, status: :accepted
+      render json: { user: @user, jwt: @token }, status: :accepted
     else
       render json: { message: 'Invalid login' }, status: :unauthorized
     end
