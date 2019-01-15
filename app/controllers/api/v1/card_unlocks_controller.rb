a9523ddac838c91e461122d4f0cf28b5e8e2cb25
brewchetta <chett.tiller@gmail.com>
2019-01-15 14:49:48 -0500

    Change to card unlocks to accept proper params

--- app/controllers/api/v1/card_unlocks_controller.rb
+++ app/controllers/api/v1/card_unlocks_controller.rb
@@ -10,7 +10,14 @@ class Api::V1::CardUnlocksController < ApplicationController
     render json: {card_unlock: @card_unlock, status: :ok}
   end
 
-
+  def create
+    @card_unlock = CardUnlock.create(card_unlock_params)
+    if @card_unlock.valid?
+      render json: { card_unlock: @card_unlock, status: :created }
+    else
+      render json: { errors: @card_unlock.errors.full_messages[0], status: :not_acceptable }
+    end
+  end
 
   private
 
@@ -19,7 +26,7 @@ class Api::V1::CardUnlocksController < ApplicationController
   end
 
   def card_unlock_params
-    params.require(:card_unlock).permit(:user_id)
+    params.require(:card_unlock).permit(:user_id, :card_id)
   end
 
 end
